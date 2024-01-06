6f6a73eab9a27abff7bde0aaa2be77b033f56ef6
augustinebest <augustusbest00@gmail.com>
2024-01-06 21:03:20 +0000

    Added maxProfit Algo

---
+++ January/maxProfit.js
@@ -0,0 +1,27 @@
+// Level: Easy
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+var maxProfit = function (prices) {
+  if (prices.length <= 0) {
+    return 0;
+  }
+
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    const potentialProfit = prices[i] - minPrice;
+
+    maxProfit = Math.max(maxProfit, potentialProfit);
+    minPrice = Math.min(minPrice, prices[i]);
+  }
+
+  return maxProfit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
